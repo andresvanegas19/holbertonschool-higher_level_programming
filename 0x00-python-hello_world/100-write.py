6f498b1797e196ad2d383d4762147f25d918032d
Andres reyes <andressantiagore@gmail.com>
2020-04-27 18:14:50 -0500

    [fix]: import the text in the file and change the sys in 100

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,2 +1,3 @@
 import sys
-sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19")
+sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+exit(1)
--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,5 +1,1 @@
-print("""The Zen of Python
-
-Beautiful is better than ugly.
-Explicit is better than implicit.
-""")
+import this

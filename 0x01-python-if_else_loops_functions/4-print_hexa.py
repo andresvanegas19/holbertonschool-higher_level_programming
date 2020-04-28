2a48b92b5fd9cb88262f9636b396e8028a68c2a0
Andres reyes <andressantiagore@gmail.com>
2020-04-28 15:42:05 -0500

    [upload]: up the main script in python

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for hexa in range(0, 99):
+for hexa in range(0, 98):
     # hex(hexa)
     print("{:d} = 0x{:x}".format(hexa, hexa))
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(words):
+    for word in words:
+        if ((ord(word) >= 97) and (ord(word) <= 122)):
+            word = chr(ord(word) - 32)
+        print("{}".format(word), end ="")
+    print("")

ce3d17b74fb0acca172fc31e8805cf38c491f077
Andres reyes <andressantiagore@gmail.com>
2020-04-28 10:27:53 -0500

    [fix]: change the script to print without spaces

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,19 +9,6 @@ if (number % 10 is not 0):
         else:
             print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, number % 10))
     else:
-
+            print("Last digit of {:d} is -{:d} and is less than 6 and not 0".format(number, (number* (-1)) % 10))
 else:
     print("Last digit of {:d} is {:d} and is 0".format(number, number % 10))
-
-
-#      if ((number > 0) and (number % 10 > 5)):
-#        if (number < 5):
-#            print("Last digit of {:d} is {:d} and is greater than 5".format(number, number % 10))
-#        else:
-#            print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, number % 10))
-#    else:
-#        if (number % 10 <= 5):
-#            print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, number % 10))
-#        else:
-#            print("Last digit of {:d} is -{:d} and is less than 6 and not 0".format(number, (number* (-1)) % 10))
-#

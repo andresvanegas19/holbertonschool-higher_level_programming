e2dc86ce57d02eac2ccdf48613f79a7e3dc7cd88
Andres reyes <andressantiagore@gmail.com>
2020-04-28 12:22:25 -0500

    [fix]: change spaces and delete the way to print

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for hexa in range(0, 100):
-    # hex(hexa) 0x{0:02x}
-    print("{:d} = {}".format(hexa, hex(hexa)))
+    # hex(hexa)
+    print("{:d} = 0x{:x}".format(hexa, hexa))

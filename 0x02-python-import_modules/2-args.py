132cfd13b2edf193dccb9ca2e6b7aa01a6b8464b
Andres reyes <andressantiagore@gmail.com>
2020-04-30 16:49:35 -0500

    [add]: fix the conditional and edit the gitignore

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,11 @@ if __name__ == "__main__":
     if (len(s.argv) is 1):
         print("0 arguments.")
     else:
-        print("{:d} argument:".format(len(s.argv)))
-        for word, num in zip(s.argv, range(0,len(s.argv))):
-            if (num is not 0):
-                print("{:d}: {}".format(num, word))
+        if (len(s.argv) is 2):
+            print("{:d} argument:".format(len(s.argv) - 1))
+            print("1: {}".format(s.argv[1]))
+        else:
+            print("{:d} arguments:".format(len(s.argv) - 1))
+            for word, num in zip(s.argv, range(0,len(s.argv))):
+                if (num is not 0):
+                    print("{:d}: {}".format(num, word))

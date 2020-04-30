9d98501b7a2162d33eb1771a0013e0cb7f2d60c7
Andres reyes <andressantiagore@gmail.com>
2020-04-30 17:10:56 -0500

    [add]: remove the alias and modifi to func

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import calculator_1 as calc
+import calculator_1 as func
 
 if __name__ == "__main__":
     a = 10
     b = 5
-    print('{:d} + {:d} = {:d}'.format(a, b, calc.add(a, b)), end='\n')
-    print('{:d} - {:d} = {:d}'.format(a, b, calc.sub(a, b)), end='\n')
-    print('{:d} * {:d} = {:d}'.format(a, b, calc.mul(a, b)), end='\n')
-    print('{:d} / {:d} = {:d}'.format(a, b, calc.div(a, b)), end='\n')
+    print('{:d} + {:d} = {:d}'.format(a, b, func.add(a, b)))
+    print('{:d} - {:d} = {:d}'.format(a, b, func.sub(a, b)))
+    print('{:d} * {:d} = {:d}'.format(a, b, func.mul(a, b)))
+    print('{:d} / {:d} = {:d}'.format(a, b, func.div(a, b)))

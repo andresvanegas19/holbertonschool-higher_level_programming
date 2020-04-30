400272abee6a97ad2de3b1c88db5ae68fdb404ba
Andres <andressantiagore@gmail.com>
2020-04-30 21:31:21 +0000

    [upload]: up the new script and delete the support example

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-print(dir(hidden_4))
+import hidden_4 as hid
+
+for function in dir(hid):
+    if function[1] is not '_' and function[-1] is not '_':
+        print(function)

88038157652997296663fb517bad362d05305b42
Andres reyes <andressantiagore@gmail.com>
2020-05-05 14:02:19 -0500

    [fix]: add the conditional to empty string

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    if (not sentence):
+        first_word = None
+    else:
+        first_word = sentence[0]
     lenght = len(sentence)
-    first_word = sentence[0]
     new_tople = (lenght, first_word)
     return (new_tople)

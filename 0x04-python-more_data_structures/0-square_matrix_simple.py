a3673f5f90b6007172746088eddd7d2280a246f4
Andres reyes <andressantiagore@gmail.com>
2020-05-07 14:26:23 -0500

    [fix]: change the lambda and delete the space at the end

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     if not matrix:
         return matrix
-    square = list(map(lambda arr: list(map(lambda num: num * 4, arr)), matrix))
+    square = list(map(lambda arr: list(map(lambda num: num ** 2, arr)), matrix))
 #    a = [[0 for x in range(n)] for x in range(m)]
     return square

20f22e5e484e8f4d8bd3ac14415448a98e67323f
Andres reyes <andressantiagore@gmail.com>
2020-05-07 12:24:42 -0500

    [fix]: add the script with map

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,5 @@
+#!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    square = list(map(lambda arr: list(map(lambda num: num * 4, arr)), matrix))
+#    a = [[0 for x in range(n)] for x in range(m)]
+    return square
--- 0x04-python-more_data_structures/apuntes.py
+++ 0x04-python-more_data_structures/apuntes.py
@@ -1,18 +1,45 @@
-def fahrenheit(T):
-    return ((float(9)/5)*T + 32)
+def get_function_mul(num):
+    def multi(value):
+        return num * value
 
-def celsius(T):
-    return (float(5)/9)*(T-32)
-
-temperatures = (36.5, 37, 37.5, 38, 39)
-
-F = map(fahrenheit, temperatures)
-C = map(celsius, F)
+    return multi
 
 
-temperatures_in_Fahrenheit = list(map(fahrenheit, temperatures))
-temperatures_in_Celsius = list(map(celsius, temperatures_in_Fahrenheit))
-print(temperatures_in_Fahrenheit)
-[97.7, 98.60000000000001, 99.5, 100.4, 102.2]
-print(temperatures_in_Celsius)
-[36.5, 37.00000000000001, 37.5, 38.00000000000001, 39.0]
+def div(num):
+    return num / 2
+
+
+save_func = get_function_mul(5)
+
+print("{}".format(save_func(10)))
+
+list_functions = [div, get_function_mul]
+
+print(list_functions[0](10))
+
+# dict is for dictionary
+
+print(dir(dict))
+
+circle_areas = [3.56773, 5.57668, 4.00914, 56.24241, 9.01344, 32.00013]
+result = list(map(round, circle_areas, range(1, 7)))
+# round necesita dos argumento, circle y range son sus argumentos
+# map lo que hace es iterar sobre cada elemento
+# circle_areas son los numeros y que se itera por cada numero y range es el numero de decimales
+# el numero de decimales se va ir mostrando mientras itere el numero de range
+# entonces map puede iterar varias variables al tiempo, pero si alguna de las listas que
+# esta iterando se detiene, no va mas alla y la funcion para
+
+
+#numeros = lambda x: x + x
+# es mejor no usar lambda, en vez de eso usar
+# usar una funcion normal, las lambda se usan cuando no la vamos utilizar mas de una vez
+c = map(lambda x: x + x, filter(lambda x: (x >= 3), (1, 2, 3, 4)))
+
+print(list(c))
+
+# Dictionary with three keys
+Dictionary1 = {'A': 'Geeks', 'B': 'For', 'C': 'Geeks'}
+
+# Printing keys of dictionary
+print(Dictionary1.keys())

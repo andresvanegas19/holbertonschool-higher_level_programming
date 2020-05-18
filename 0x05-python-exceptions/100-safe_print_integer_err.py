addc1ea4e52521ffdc3b97845f8a428024e2e819
Andres reyes <andressantiagore@gmail.com>
2020-05-18 13:34:51 -0500

    upload the change and print in different way

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,9 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print('{:d}'.format(value))
-    except (ValueError, TypeError):
-        sys.stderr \
-            .write('Exception: Unknown format code'
-                   ' \'d\' for object of type \'str\'\n')
+    except (ValueError, TypeError) as err:
+        print(err, file=sys.stderr)
         return False
     return True

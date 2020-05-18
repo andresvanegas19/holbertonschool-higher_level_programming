06ddd610e2d20890b57757f0b43c7771cb366391
Andres reyes <andressantiagore@gmail.com>
2020-05-18 12:35:35 -0500

    add the format in the print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     for num in range(x):
         if num < x:
             try:
-                print("{}".format(my_list[num]), end='')
+                print("{:d}".format(my_list[num]), end='')
                 total += 1
             except IndexError:
                 print()

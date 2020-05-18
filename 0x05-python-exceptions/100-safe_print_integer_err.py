ee10ac627ec33bab705910ad70f1f518567ff119
Andres reyes <andressantiagore@gmail.com>
2020-05-18 13:39:06 -0500

    fix and write with the exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,7 @@ import sys
 def safe_print_integer_err(value):
     try:
         print('{:d}'.format(value))
-    except (ValueError, TypeError) as err:
-        print(err, file=sys.stderr)
+    except ValueError as err:
+        print('Exception: {}'.format(err), file=sys.stderr)
         return False
     return True

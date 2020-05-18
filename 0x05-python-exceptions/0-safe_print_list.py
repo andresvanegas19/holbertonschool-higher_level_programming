f2dcd991ea2c20692991586576f467cf1d361507
Andres reyes <andressantiagore@gmail.com>
2020-05-18 12:32:19 -0500

    fix, add a variable to count the iteration

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if not my_list:
+    if not my_list or x < 0:
         return 0
-    for num in range(x + 1):
-        if num <= x:
+    total = 0
+    for num in range(x):
+        if num < x:
             try:
                 print("{}".format(my_list[num]), end='')
+                total += 1
             except IndexError:
                 print()
-                return num
+                return total - 1
     print()
-    return num
+    return total

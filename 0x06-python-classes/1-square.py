d3094ca27a8069bc8f90d65a7042986e4084a758
Andres reyes <andressantiagore@gmail.com>
2020-05-19 16:50:37 -0500

    change the code to one

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Class that contain a size of the square
+    """
+    def __init__(self, size):
+        """ __init__ method.
+                The __init__ method may be documented in either the class level
+                docstring, or as a docstring on the __init__ method itself.
+
+                Args:
+                    size (str): is the size of the square
+        """
+        self.__size = size

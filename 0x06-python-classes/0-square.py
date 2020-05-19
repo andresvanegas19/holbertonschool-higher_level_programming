61f2e12ba6ef0c8d0e9e84299c99d1921168d91d
Andres reyes <andressantiagore@gmail.com>
2020-05-19 17:17:12 -0500

    add more comments

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+""" This is the class of the square"""
+
+
 class Square:
+    """
+    Class that contain a size of the square
+    """
     pass
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,7 +3,7 @@ class Square:
     """
     Class that contain a size of the square
     """
-    def __init__(self, size):
+    def __init__(self, size=0):
         """ __init__ method.
                 The __init__ method may be documented in either the class level
                 docstring, or as a docstring on the __init__ method itself.

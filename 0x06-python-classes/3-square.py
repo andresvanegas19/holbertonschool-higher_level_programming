20b0af75886b5bfd081eb553f96abd6bfcdae49a
Andres reyes <andressantiagore@gmail.com>
2020-05-19 17:50:40 -0500

    add the method to the file

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+class Square:
+    """
+    Class that contain a size of the square
+    """
+
+    def __init__(self, size=0):
+        """ __init__ method.
+                The __init__ method may be documented in either the class level
+                docstring, or as a docstring on the __init__ method itself.
+
+                Args:
+                    size (str): is the size of the square
+        """
+        self.__size = size
+
+    @property
+    def validate_num(self):
+        return self.__size
+
+    @validate_num.setter
+    def validate_num(self, size):
+        if isinstance(size, int):
+            self.__size = size
+        elif size < 0:
+            raise ValueError('size must be an integer')
+        else:
+            raise TypeError('size must be >= 0')

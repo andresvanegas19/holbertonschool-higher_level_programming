0d29e4b13543567ec350fea82bd5e11ad57422ba
andres reyes <andressantiagore@gmail.com>
2020-06-02 18:49:06 -0500

    add tet

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -7,7 +7,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Square(BaseGeometry):
     """ it seems is a figure that it will be a sum of an operatiom"""
     def __init__(self, size):
-        self.__size = super().integer_validator("size", size)
+        self.__size = integer_validator("size", size)
 
     def area(self):
         """calculate the area of the class"""
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -6,11 +6,15 @@ class BaseGeometry:
     """function that validated if the object is
     a instace of a class in python"""
 
+    def __init__(self):
+        self.name = None
+
     def area(self):
         """Just raise a error"""
         raise Exception('area() is not implemented')
 
-    def integer_validator(self, name, value):
+    @staticmethod
+    def integer_validator(name, value):
         """ that validates value, name is a string and verificated value"""
         if type(value) is not int:
             raise Exception('{} must be an integer'.format(name))

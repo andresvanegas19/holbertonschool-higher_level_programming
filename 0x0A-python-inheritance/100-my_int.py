31641091da5a3ee27733763d193c8773e0fa2102
andres reyes <andressantiagore@gmail.com>
2020-06-02 17:48:45 -0500

    upload the class and modificiated

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" a wirdo class"""
+
+
+class MyInt(int):
+    """this invert a string"""
+    def __init__(self, value):
+        if type(value) is not str:
+            self.__value = str(value)
+        else:
+            self.__value = int(value)
+
+    def __str__(self):
+        return self.__value
+
+
+

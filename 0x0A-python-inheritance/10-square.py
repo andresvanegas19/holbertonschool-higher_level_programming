80802c8d47e282ae3dd5bbf31c078e1dc93ba6cf
andres reyes <andressantiagore@gmail.com>
2020-06-02 18:10:48 -0500

    add a class method

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+"""Test if the object is inherent of a class"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(BaseGeometry):
+    """ it seems is a figure that it will be a sum of an operatiom"""
+    def __init__(self, size):
+        self.__size = super().integer_validator("size", size)
+
+    def area(self):
+        """calculate the area of the class"""
+        return self.__size * self.__size
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,6 +5,7 @@
 class BaseGeometry:
     """function that validated if the object is
     a instace of a class in python"""
+
     def area(self):
         """Just raise a error"""
         raise Exception('area() is not implemented')

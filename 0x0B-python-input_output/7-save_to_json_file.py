3312f44fd507e8110213b1064b92652f6e2b8bba
andres reyes <andressantiagore@gmail.com>
2020-06-03 17:23:29 -0500

    delete a space

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -7,4 +7,3 @@ def save_to_json_file(my_obj, filename):
     """writes an Object to a text file, using a JSON representation"""
     with open(filename, 'w', encoding='utf-8') as fl:
         fl.write(json.dumps(my_obj))
-

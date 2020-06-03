3ed7af8c42ea77b75f2d7e460f93fdadcf15af44
andres reyes <andressantiagore@gmail.com>
2020-06-03 18:07:55 -0500

    finish the last script

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,4 +3,14 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """insert text in the exact position"""
+    count = 0
+    with open(filename, "r+") as f:
+        text = f.readlines()
+    for letter in text:
+        count += 1
+        if search_string in letter:
+            text.insert(count, new_string)
 
+    with open(filename, 'w') as f:
+        f.write(''.join(text))

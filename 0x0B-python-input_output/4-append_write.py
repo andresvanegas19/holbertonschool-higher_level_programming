89e0326d15a7f9a79d1b78d19f181ada350959c4
andres reyes <andressantiagore@gmail.com>
2020-06-03 17:21:33 -0500

    add comments

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -3,5 +3,7 @@
 
 
 def append_write(filename="", text=""):
+    """appends a string at the end of a text file (UTF8) and
+     returns the number of characters added:"""
     with open(filename, 'a', encoding='utf-8') as fl:
         return fl.write(text)

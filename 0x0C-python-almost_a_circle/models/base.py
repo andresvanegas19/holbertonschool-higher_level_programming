e7cf298d7aa210a272934ddd20caa01668d9a4dc
andres reyes <andressantiagore@gmail.com>
2020-06-06 18:14:51 -0500

    upload the main function

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """This class will be the “base” of all other classes in this project.
 The goal of it is to manage id attribute in all your future classes
 and to avoid duplicating the same code (by extension, same bugs)"""
+import json
 
 
 class Base:
@@ -14,3 +15,11 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """is a  the static method  that
+        :return the JSON string representation of list_dictionaries"""
+        if list_dictionaries:
+            return json.dumps(list_dictionaries)
+        return '[]'

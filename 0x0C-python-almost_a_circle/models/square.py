3aa4d9ccb429dc9365130f83b406db3cc657e8c5
andres reyes <andressantiagore@gmail.com>
2020-06-06 17:43:47 -0500

    finish the square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,9 +4,38 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """ a kinf of figure but it has another specifics"""
+    # def __init__(self, width, height, x=0, y=0, id=None):
     def __init__(self, size, x=0, y=0, id=None):
-        Rectangle.__init__(self, size, size, x, y, id)
+        super().__init__(size, size, x, y, id)
+        self._size = size
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        super().evaluated(value, 'width')
+        self._size = value
 
     def __str__(self):
+        # [Square] (<id>) <x>/<y> - <size>
         return '[Square] ({}) {}/{} - {}'.format(self.id, self.x,
-                                         self.y, self.width, self.height)
+                                                 self.y, self._size)
+
+    def update(self, *args, **kwargs):
+        """that assigns an argument to each attribute and update it"""
+        a = [self.id, self.size, self.x, self.y]
+        err = ['id', 'size', 'x', 'y']
+        if args:
+            for num in range(len(args)):
+                self.evaluated(args[num], err[num])
+                a[num] = args[num]
+        else:
+            for key, val in kwargs.items():
+                self.evaluated(val, key)
+                if key in err:
+                    a[err.index(key)] = val
+        # to unpacking the variables and save it
+        self.id, self.size, self.x, self.y = a

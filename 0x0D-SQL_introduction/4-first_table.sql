3dce4152cb4f58b842aefa499b7941a3f9ae2196
andres reyes <andressantiagore@gmail.com>
2020-07-13 21:25:59 -0500

    Upload the query

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -3,22 +3,3 @@ CREATE TABLE IF NOT EXISTS first_table(
      id INT,
      name VARCHAR(256)
 );
--- Rename the organization column to id
-ALTER TABLE organizations
-RENAME COLUMN organization TO id;
-
--- Make id a primary key
-ALTER TABLE organizations
-ADD CONSTRAINT organization_pk PRIMARY KEY (id);
-
-
--- Add the new column to the table
-ALTER TABLE professors
-ADD COLUMN id serial;
-
--- Make id a primary key
-ALTER TABLE professors
-ADD CONSTRAINT professors_pkey PRIMARY KEY (id);
-
--- Have a look at the first 10 rows of professors
-SELECT * FROM professors LIMIT 10;

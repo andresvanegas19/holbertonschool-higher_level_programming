1d55b679107c653aaf51072d3dca98c3f4b2345a
andres reyes <andressantiagore@gmail.com>
2020-07-13 23:14:08 -0500

    change the script dont use the SOURCE

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,11 +1,5 @@
 -- displays the average temperature
 -- (Fahrenheit) by city ordered by temperature (descending).
-
--- first create a database to hold the tables
-CREATE DATABASE IF NOT EXISTS hbtn_0;
-USE hbtn_0;
-SOURCE database/temperatures.sql;
-
 -- manipulate the data
 SELECT city, AVG(value) AS avg_temp FROM temperatures
 GROUP BY 1 ORDER BY 2 DESC;

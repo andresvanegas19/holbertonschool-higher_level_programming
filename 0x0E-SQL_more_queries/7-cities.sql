c8420114e0ce4a4a975cb7b20c02af51ca86c44f
andres reyes <andressantiagore@gmail.com>
2020-07-14 16:50:39 -0500

    change the comments

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,7 +1,6 @@
-/*
-     creates the database hbtn_0d_usa and
-     the table cities (in the database hbtn_0d_usa) on MySQL server.
-*/
+-- creates the database hbtn_0d_usa and
+-- the table cities (in the database hbtn_0d_usa) on MySQL server.
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(

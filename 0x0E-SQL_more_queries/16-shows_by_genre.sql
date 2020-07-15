09fddc555eab156a48c3672e54a1bfd1fa846c4d
andres reyes <andressantiagore@gmail.com>
2020-07-14 19:00:54 -0500

    fix and order by 1 and 2

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,11 +1,11 @@
--- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
--- tv_shows.title - tv_genres.name
+-- lists all tv_shows, and all genres linked to that show, from the database hbtn_0d_tvtv_shows.
+-- tv_tv_shows.title - tv_genres.name
 
-SELECT shows.title, genres.name
+SELECT tv_shows.title, tv_genres.name
 FROM tv_show_genres
-RIGHT JOIN tv_shows AS shows
-     ON tv_show_genres.show_id = shows.id
-LEFT JOIN tv_genres AS genres
-     ON tv_show_genres.genre_id = genres.id
+RIGHT JOIN tv_shows
+     ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres
+     ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY 1;
 

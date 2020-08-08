54db86846523f5071c88f2a0024351d4a96b3948
andres reyes <andressantiagore@gmail.com>
2020-08-08 16:29:14 +0000

    give format to the files

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This script execute in env of sqlalchemy
+    and execute"""
+
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """This is for a structure to save in Tables and use
+    ORM to manipulated them"""
+    __tablename__ = 'states'
+    id = Column(
+        'id',
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True)
+    name = Column('name', String(128), nullable=False)

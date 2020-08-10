e60bc952b7773efcc27a6e01f5ea2252ff535102
andres reyes <andressantiagore@gmail.com>
2020-08-10 01:02:15 +0000

    upload script to print the states

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.engine.url import URL
+
+if __name__ == "__main__" and len(sys.argv) == 4:
+    db_uri = {
+        'drivername': 'mysql',
+        'username': sys.argv[1],
+        'password': sys.argv[2],
+        'host': 'localhost',
+        'port': 3306,
+        'database': sys.argv[3]
+    }
+
+    Session = sessionmaker(autoflush=True)
+    engine = create_engine(URL(**db_uri), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session.configure(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by('id').all()
+
+    for state in states:
+        print('{}: {}'.format(state.id ,state.name))
+        cities = session.query(City).filter(City.state_id == state.id) \
+            .order_by('id').all()
+        for city in cities:
+            print('    {}: {}'.format(city.id, city.name))
+
+    session.close()

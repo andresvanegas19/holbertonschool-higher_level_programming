3d0e4abf05a811543a3490d271c1385203ef0a37
andres reyes <andressantiagore@gmail.com>
2020-08-24 22:04:52 -0500

    upload script and change the handle the print

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,8 +6,5 @@ if __name__ == '__main__':
     import requests
     import sys
     url = sys.argv[1]
-    try:
-        r = requests.post(url)
-        print(r.raise_for_status())
-    except requests.exceptions.RequestException as e:
-        print("Error code: {}".format(e.__str__().split()[0]))
+    r = requests.post(url, data={'email': sys.argv[2]})
+    print(r.text)

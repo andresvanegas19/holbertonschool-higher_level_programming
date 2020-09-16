2b96af5a42173351002d03bad102af0e746fd5b8
andres reyes <andressantiagore@gmail.com>
2020-09-15 19:51:20 -0500

    change the function

--- 0x12-javascript-warm_up/forloop.js
+++ 0x12-javascript-warm_up/forloop.js
@@ -5,3 +5,11 @@ const phrases = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 phrases.forEach(phrase => {
   console.log(phrase);
 });
+
+const factorial = (number) => {
+  if (number === 0 || number === 1) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
+  }
+};

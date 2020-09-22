07ac3683fb910e83d8ab8d291a9a178456345cd8
andres reyes <andressantiagore@gmail.com>
2020-09-22 18:23:00 -0500

    upload the way to manipulate the err

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,26 +1,13 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 const { argv } = require('process');
 const fs = require('fs');
 
 const file = argv[2];
-
-try {
-  const fileResult = fs.readFileSync(file, 'utf-8');
-  process.stdout.write(fileResult.toString())
-} catch (err) {
-  // If the type is not what you want, then just throw the error again.
-  if (err.code === 'ENOENT') {
-    console.log({
-    'Error': 'ENOENT: no such file or directory, open \'' + file + '\'' + 'at Error (native)',
-    'errno': -2,
-    'code': 'ENOENT',
-    'syscall': 'open',
-    path: 'doesntexist' });
+fileSystem.readFile(file, 'utf-8', function (error, data) {
+  if (error) {
+    console.log(error);
   } else {
-    throw err;
+    console.log(data);
   }
-
-  // Handle a file-not-found error
-}
-
+});
 

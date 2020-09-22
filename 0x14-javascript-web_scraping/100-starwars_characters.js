b138f6d7f9db0f49af638111a251faf51acee16d
andres reyes <andressantiagore@gmail.com>
2020-09-22 18:39:09 -0500

    upload files

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,20 +3,23 @@ const request = require('request');
 const { argv } = require('process');
 
 const options = {
-    url: `https://swapi-api.hbtn.io/api/films/${argv[2]}/`,
-    method: 'GET',
-    headers: {
-        'Accept-Charset': 'utf-8',
-    }
+  url: `https://swapi-api.hbtn.io/api/films/${argv[2]}/`,
+  method: 'GET',
+  headers: {
+    'Accept-Charset': 'utf-8'
+  }
 };
 
-request(options, function(err, res, body) {
+request(options, function (err, res, body) {
+  if (err) throw err;
+
   const characters = JSON.parse(body).characters;
 
   characters.forEach(element => {
-    request(element, function(err, res, _body) {
-      console.log(JSON.parse(_body).name)
-    })
-  });
+    request(element, function (err, res, _body) {
+      if (err) throw err;
 
+      console.log(JSON.parse(_body).name);
+    });
+  });
 });
--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,26 +3,28 @@ const request = require('request');
 const { argv } = require('process');
 
 const options = {
-    url: argv[2],
-    method: 'GET',
-    headers: {
-        'Accept-Charset': 'utf-8',
-    }
+  url: argv[2],
+  method: 'GET',
+  headers: {
+    'Accept-Charset': 'utf-8'
+  }
 };
 
-request(options, function(err, res, body) {
-  const result = JSON.parse(body)
+request(options, function (err, res, body) {
+  if (err) throw err;
+
+  const result = JSON.parse(body);
   const dict = {};
 
   result.forEach(element => {
-    let userId = element['userId']
+    const userId = element.userId;
     if (!dict[userId]) {
-      dict[userId] = 0
+      dict[userId] = 0;
     }
 
-    if (element['completed'] === true) {
-      dict[userId] += 1
+    if (element.completed === true) {
+      dict[userId] += 1;
     }
   });
-  console.log(dict)
+  console.log(dict);
 });

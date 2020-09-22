c5290fe07510d381cce9b88c61bd469eece97348
andres reyes <andressantiagore@gmail.com>
2020-09-22 18:34:47 -0500

    cahnge the way to print and control errors

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,24 +3,24 @@ const request = require('request');
 const { argv } = require('process');
 
 const options = {
-    url: argv[2],
-    method: 'GET',
-    headers: {
-        'Accept-Charset': 'utf-8',
-    }
+  url: argv[2],
+  method: 'GET',
+  headers: {
+    'Accept-Charset': 'utf-8'
+  }
 };
 
 const wedgeAntilles = 'https://swapi-api.hbtn.io/api/people/18/';
-request(options, function(err, res, body) {
+request(options, function (err, res, body) {
+  if (err) throw err;
+
   let total = 0;
-  const result = JSON.parse(body)
+  const result = JSON.parse(body);
 
   result.results.forEach(element => {
-    // console.log(element.characters)
     if (element.characters.indexOf(wedgeAntilles) > -1) {
       total++;
     }
   });
   console.log(total);
 });
-

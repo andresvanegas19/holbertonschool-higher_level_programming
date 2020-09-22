679530da94de2c3a098bcec212b179307d2e37d2
andres reyes <andressantiagore@gmail.com>
2020-09-21 21:31:46 -0500

    add the style to the file

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,33 +2,32 @@
 const { exit, cwd, argv } = require('process');
 const fs = require('fs');
 
-if (argv.length != 5) {
-  exit(0)
+if (argv.length !== 5) {
+  exit(0);
 }
 
-const path = cwd() + '/'
-const file_a = path + argv[2]
-const file_b = path + argv[3]
+const path = cwd() + '/';
+const fileA = path + argv[2];
+const fileB = path + argv[3];
 
-const res_file_a = fs.readFileSync(file_a, function (err) {
-    if(err) {
-        return console.log(err);
-    }
+const resFileA = fs.readFileSync(fileA, function (err) {
+  if (err) {
+    return console.log(err);
+  }
 }).toString()
-  .replace(/(\r\n|\n|\r)/gm,"");
+  .replace(/(\r\n|\n|\r)/gm, '');
 
-const res_file_b = fs.readFileSync(file_b, function (err) {
-    if(err) {
-        return console.log(err);
-    }
+const resFileB = fs.readFileSync(fileB, function (err) {
+  if (err) {
+    return console.log(err);
+  }
 }).toString()
-  .replace(/(\r\n|\n|\r)/gm,"");
+  .replace(/(\r\n|\n|\r)/gm, '');
 
-
-total = res_file_a + '\n' + res_file_b + '\n'
+const total = resFileA + '\n' + resFileB + '\n';
 
 fs.writeFile(path + argv[4], total, function (err) {
-    if(err) {
-        return console.log(err);
-    }
+  if (err) {
+    return console.log(err);
+  }
 });

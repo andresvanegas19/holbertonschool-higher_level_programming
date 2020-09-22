dbddd19e04be9575ce82da6a3395701969bf1586
andres reyes <andressantiagore@gmail.com>
2020-09-22 18:35:43 -0500

    add style to the file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,14 +4,16 @@ const { argv } = require('process');
 const fs = require('fs');
 
 const options = {
-    url: argv[2],
-    method: 'GET',
-    headers: {
-        'Accept-Charset': 'utf-8',
-    }
+  url: argv[2],
+  method: 'GET',
+  headers: {
+    'Accept-Charset': 'utf-8'
+  }
 };
 
-request(options, function(err, res, body) {
+request(options, function (err, res, body) {
+  if (err) throw err;
+
   fs.writeFile(argv[3], body, function (err) {
     if (err) {
       return console.log(err);

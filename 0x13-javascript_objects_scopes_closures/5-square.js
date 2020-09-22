1cdfa644dc4fc9a3e8c2f9764f061c077ef7e8b6
andres reyes <andressantiagore@gmail.com>
2020-09-21 23:11:50 -0500

    modificated the class base

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle.js');
+const Rectangle = require("./4-rectangle.js");
 
-module.exports = class Square extends Square5 {
+module.exports = class Square extends Rectangle{
   constructor (size) {
-    super(size, size);
-    this.size = size;
+    super (size, size);
   }
 };
